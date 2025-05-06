c0a4692a7d802a3ff51462962607934a073d7fef
DESKTOP-RK0B994\pysnow <vasti009@gmail.com>
2025-05-06 03:49:53 -0300

    Prep for ingame consoles auto importing scripts

--- script/game_script.gd
+++ script/game_script.gd
@@ -1,5 +1,13 @@
 extends RefCounted
 class_name NoteGameScript
 
+var tree: SceneTree
+
+func script_name() -> String:
+	return ""
+
+func script_documentation() -> String:
+	return ""
+
 func execute(params: Array):
 	pass

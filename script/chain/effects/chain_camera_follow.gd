ce26abbf06c5ee93d62f6330624e425e1bb2c75b
DESKTOP-RK0B994\pysnow <vasti009@gmail.com>
2025-05-13 20:13:07 -0300

    Forgot to remove debug prints.

--- script/chain/effects/chain_camera_follow.gd
+++ script/chain/effects/chain_camera_follow.gd
@@ -3,10 +3,7 @@ class_name ChainNoteCameraFollow
 
 ## This interacts with the scene's NoteCamera2D and makes it follow the context.
 
-
 func _start(data):
 	var cam = get_viewport().get_camera_2d()
-	print(cam.name)
 	if cam is NoteCamera2D:
-		print("Hello???")
 		cam.follow(data)

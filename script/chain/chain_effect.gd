94c3ab7c074a0f212be94764b746e2b037c3e62e
Josiah P. <vasti009@gmail.com>
2025-11-12 17:55:04 -0400

    Ported the chain node system back over to the new version.

---
+++ script/chain/chain_effect.gd
@@ -0,0 +1,13 @@
+@icon("res://addons/note/texture/icon/effect_chain.svg")
+extends ChainNode
+class_name ChainFX
+
+## Virtual this is called to begin a chain node
+func _start(data):
+	pass
+## Virtual this is called when cancelled, to clean up earlier than expected.
+func _clean():
+	pass
+## Virtual this is called to query if the node is finished. Return true if finished.
+func _done() -> bool:
+	return true

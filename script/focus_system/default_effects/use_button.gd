459c308e206afa2c50ba918004b856076492a671
Josiah P. <vasti009@gmail.com>
2025-12-15 01:46:47 -0400

    Forwarding the button pressed signal for controllers.

--- script/focus_system/default_effects/use_button.gd
+++ script/focus_system/default_effects/use_button.gd
@@ -1,10 +1,13 @@
 extends FocusEffect
 class_name UseButtonFocusEffect
 
+signal pressed
+
 func focus_confirm(down: bool, long: bool = false) -> bool:
 	var parent = get_parent()
 	if parent is Button:
 		if down and !parent.disabled:
 			parent.pressed.emit()
+			pressed.emit()
 			return true
 	return false
